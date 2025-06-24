623e26bc4ea098e3cc3bc1a337339fefc6eba18f
Agustin <cittadini.agustin@gmail.com>
2025-06-24 09:06:38 -0300

    segunda entrega2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemCount from './components/ItemCount'
-import ItemListContainer from './components/ItemListContainer';
+import ItemListContainer from './components/ItemListContainer'
 import NavBar from './components/NavBar'
 import ItemDetailContainer from './components/ItemDetailContainer'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Item = ({prod}) => {
     const {name, price, img}= prod
@@ -8,7 +9,7 @@ const Item = ({prod}) => {
   <div className="card-body">
     <h5 className="card-title">{name}</h5>
     <p className="card-text">${price},00</p>
-    <a href="#" className="btn btn-primary">Ver más</a>
+    <Link to={`/item/${id}`} className="btn btn-primary">Ver más</Link>
   </div>
 </div>
   )
